563b2042645c256649a21024e24ce17caf0b42bd
Kevin Lemus <klemus@snecorp.com>
2023-08-17 17:06:47 -0600

    Includes new logic

--- src/infrastructure/repository/empty_return_impl.py
+++ src/infrastructure/repository/empty_return_impl.py
@@ -13,7 +13,7 @@ class EmptyReturnImpl(EmptyReturnABC):
         self.__enviroment: ENVIRONMENT = stage
         self.eg_client: EventGridImpl = None
         self.__credential: str= "EVENT-GRID-ACCESS-KEY-EMPTY-RETURN" 
-        self.__endpoint: str= "EVENT-GRID-ENDPOINT-EMPTY-RETURN"
+        self.__endpoint: str= "EVENT-GRID-ENDPOINT-EMPTY-RETURN" 
 
     async def __aenter__(self) -> Self:
         async with EventGridImpl(self.__enviroment, self.__credential, self.__endpoint) as client:
@@ -25,11 +25,13 @@ class EmptyReturnImpl(EmptyReturnABC):
             logging.info(f"An Exception has occured {value}")
 
     async def create_empty_return(self, shipment_list: List[Shipment]):
-        shipments_id: List[str] = [shipment.ds_id for shipment in shipment_list if shipment.tmp_type == 'Import' and shipment.ds_status != 'A']
+        shipments_id: List[str] = [shipment.ds_id for shipment in shipment_list if shipment.ds_status != 'A' and shipment.has_changed_events]
+        data: dict = {
+            "shipments_id": shipments_id
+        }
         try:
-            data: dict = {
-                "shipments_id": shipments_id
-            }
-            self.eg_client.send_event(data=data)
+            if data:
+                self.eg_client.send_event(data=data)
+                
         except Exception as e:
             logging.error(f"Error in create_empty_return: {e} at {datetime.now()}")

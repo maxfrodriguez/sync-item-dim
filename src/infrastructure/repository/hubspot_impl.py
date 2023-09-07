94a4aadcb9850437a529c448e1fd59408d4e46df
Kevin Lemus <klemus@snecorp.com>
2023-09-07 11:02:48 -0600

    Includes Hubspot Impl

--- src/infrastructure/repository/hubspot_impl.py
+++ src/infrastructure/repository/hubspot_impl.py
@@ -2,17 +2,18 @@ import logging
 from typing import List, Optional
 from typing_extensions import Self
 from src.domain.entities.customer import Customer
-from src.domain.repository.customer_kpi_abc import CustomerKpiABC
+from src.domain.repository.hubspot_abc import HubspotABC
 from src.infrastructure.cross_cutting.environment import ENVIRONMENT
 from src.infrastructure.cross_cutting.service_bus.service_bus_impl import ServiceBusImpl
 
 
-class CustomerKpiImpl(CustomerKpiABC):
+class HubspotImpl(HubspotABC):
+
     def __init__(self, stage) -> None:
         self.__enviroment: ENVIRONMENT = stage
         self._sb_client: ServiceBusImpl = None
         self.__sb_con_string: str= "SERVICE-BUS-CONN-STRING"
-        self.__queue_name: str= "SB-QUEUE-CUSTOMER-KPI"
+        self.__queue_name: str= "SB-QUEUE-WH-HUBSPOT"
 
     async def __aenter__(self) -> Self:
         async with ServiceBusImpl(self.__enviroment, self.__sb_con_string, self.__queue_name) as sb_client:
@@ -23,15 +24,16 @@ class CustomerKpiImpl(CustomerKpiABC):
         if exc_type is not None:
             logging.info(f"An Exception has occured {value}")
     
-    async def send_customer_kpi_sb(self, shipments_customers: List[Customer]):
+    async def send_customer_sb(self, shipments_customers: List[Customer]):
         customers: List[Customer] = []
         customer_hash: set = set()
         try:
             if shipments_customers:
                 for customer in shipments_customers:
-                    if customer.tmp and customer.customer_id and customer.template_id:
+                    if customer.tmp and customer.customer_id and customer.template_id and customer.customer_id not in customer_hash :
                         customers.append(customer)
+                        customer_hash.add(customer.customer_id)
                         
-                await self._sb_client.send_message(data=customers)
+                await self._sb_client.send_message(data=customer_hash)
         except Exception as e:
-            logging.error(f"Error in send_customer_kpi_sb: {e}")
+            logging.error(f"Error in send_customer_sb: {e}")
